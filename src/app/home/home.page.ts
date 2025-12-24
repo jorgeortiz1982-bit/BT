4034c0f74e2d1cddf6ceb43309f24e2b7ec03587
Jorge <jorgeortiz1982@hotmail.com>
2025-12-23 20:45:56 -0500

    App Ionic PWA con Bluetooth LE

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BluetoothLe } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule
+  ]
 })
 export class HomePage {
+
+  temperatura: string = '';
+
   constructor() {}
+
+  async scanAndConnect() {
+    await BluetoothLe.initialize();
+
+    const device = await BluetoothLe.requestDevice({
+      namePrefix: 'HM-10'
+    });
+
+    if (!device) return;
+
+    const deviceId = device.deviceId;
+
+    await BluetoothLe.connect({ deviceId });
+
+    await BluetoothLe.startNotifications({
+      deviceId,
+      service: 'FFE0',
+      characteristic: 'FFE1'
+    }).then((result: any) => {
+      const data = new TextDecoder().decode(result.value);
+      this.temperatura = data;
+      console.log('Temperatura:', data);
+    });
+  }
 }
